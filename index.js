df93a8230ea78c83b839b2f5525d8819de3d0fbe
Maga D. Zandaqo <denelxan@gmail.com>
2020-09-11 11:11:16 +0300

    fix: estimate time left to completion

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ class CTFB {
     const ffmpeg = exec(`ffmpeg -y -hide_banner -loglevel 16 -progress - -f concat -safe 0 -i "${this.fileListPath}" -i "${this.metaPath}" -map_metadata 1 -b:a ${this.bitrate} "${this.output}"`);
     ffmpeg.stdout.on('data', (data) => {
       const match = reCurrentTime.exec(data);
-      if (match) this.constructor.printProgress(match[1], totalTime);
+      if (match) this.constructor.printProgress(match[1], totalTime, start);
     });
     ffmpeg.on('exit', () => {
       const end = performance.now();
@@ -101,7 +101,6 @@ class CTFB {
   }
 
   static toSexagesimal(time) {
-
     const milliseconds = (time * 1000) | 0;
     const hours = this.toDecimal((milliseconds / 36e5) | 0);
     let remainder = milliseconds % 36e5;
@@ -122,11 +121,14 @@ class CTFB {
     return filename.slice(0, filename.length - extension.length);
   }
 
-  static printProgress(currentTime, totalTime) {
-    const progress = ((parseInt(currentTime, 10) / totalTime) * 100).toFixed(3) + '%';
+  static printProgress(currentPosition, totalTime, startTime) {
+    const fraction = parseInt(currentPosition, 10) / totalTime;
+    const currentTime = performance.now();
+    const progress = (fraction * 100).toFixed(3) + '%';
+    const timeLeft = this.toSexagesimal(((currentTime - startTime) * ((1 / fraction) - 1)) / 1e3);
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
-    process.stdout.write(`Progress: ${progress}    Time left: `);
+    process.stdout.write(`Progress: ${progress}    Time left: ${timeLeft}`);
   }
 
   static getCommandLine() {
